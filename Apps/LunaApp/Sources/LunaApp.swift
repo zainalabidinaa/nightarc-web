2a8bbadf7f9a217b96824060bd27421fd7e97f0d
Zain Alabidin Alkafaji <{{email_address}}>
2026-06-05 04:30:24 +0200

    refactor: inject ThemeManager, register custom font on launch

--- Apps/LunaApp/Sources/LunaApp.swift
+++ Apps/LunaApp/Sources/LunaApp.swift
@@ -5,12 +5,18 @@ import LunaCore
 struct LunaApp: App {
     @StateObject private var profileManager = ProfileManager.shared
     @StateObject private var roleManager = RoleManager.shared
+    @StateObject private var themeManager = ThemeManager.shared
+
+    init() {
+        LunaTypography.registerFonts()
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(profileManager)
                 .environmentObject(roleManager)
+                .environmentObject(themeManager)
                 .preferredColorScheme(.dark)
         }
     }
